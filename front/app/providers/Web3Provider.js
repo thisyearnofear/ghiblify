772f9673b29e0907880b150b8985d28d576d1eac
Quincybob <74205675+Quincybob@users.noreply.github.com>
2025-09-20 16:01:34 +0300

    farcaster miniapp wallet connector

--- front/app/providers/Web3Provider.js
+++ front/app/providers/Web3Provider.js
@@ -6,6 +6,7 @@ import { WagmiProvider } from "wagmi";
 import { mainnet, polygon } from "wagmi/chains";
 import { http } from "viem";
 import { farcasterFrame } from "@farcaster/frame-wagmi-connector";
+import { farcasterMiniApp } from "@farcaster/miniapp-wagmi-connector";
 import { injected, walletConnect } from "wagmi/connectors";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@rainbow-me/rainbowkit/styles.css";
@@ -89,6 +90,7 @@ const config = getDefaultConfig({
     }),
   },
   connectors: [
+    farcasterMiniApp(),
     farcasterFrame(),
     injected(),
     walletConnect({
